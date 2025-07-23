5fa7d97d309dfd0e89a5067b25ee9d4e8d2cb618
Paul Johnston <johnstonpaul801@gmail.com>
2025-07-23 15:41:34 -0600

    Made a function to update the "Random 20" Playlist

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import random
 import logging
 
 # Won't need this in production
@@ -11,21 +12,48 @@ scope = "user-library-read playlist-modify-private"
 client_id = os.getenv("SPOTIPY_CLIENT_ID")
 client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 playlist_id = os.getenv("PLAYLIST_ID")
+playlist_id_2 = os.getenv("PLAYLIST_ID_2")
+mini_playlist_id_1 = os.getenv("MINI_PLAYLIST_ID_1")
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
-hey = sp.me()
+user = sp.me()
 
 # Get latest 20 songs
-results = sp.current_user_saved_tracks()
-tracks = [item['track']['id'] for item in results['items']]
+# results = sp.current_user_saved_tracks()
+# tracks = [item['track']['id'] for item in results['items']]
 
-# Replace latest 20 songs
-sp.playlist_replace_items(playlist_id=playlist_id, items=tracks)
+# # Replace latest 20 songs
+# sp.playlist_replace_items(playlist_id=playlist_id, items=tracks)
 
-print('Replaced latest 20 songs')
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print_str = str(idx) + track['artists'][0]['name'] + " - " + track['name']
-    print(print_str)
+# Make random list of 20 liked songs
+liked_songs_count = 0
+offset = 0
+limit = 50
+k = 20
 
+while True:
+    results = sp.current_user_saved_tracks(offset=offset, limit=limit)
+    liked_songs_count += len(results['items'])
+    if results['next'] is None:
+        break
+    offset += limit
+
+print(f"Number of liked songs: {liked_songs_count}")
+
+population = range(0, liked_songs_count)
+random_integers = random.sample(population, k)
+tracks = []
+track_ids = []
+
+for rand_int in random_integers:
+    result = sp.current_user_saved_tracks(offset=rand_int, limit=1)
+    track = result['items'][0]['track']
+    tracks.append(track)
+    track_ids.append(track['id'])
+
+sp.playlist_replace_items(playlist_id=playlist_id_2, items=track_ids)
+
+logging.info('Updated random 20 songs')
+for idx, track in enumerate(tracks):
+    print_str = str(idx) + " " + track['artists'][0]['name'] + " - " + track['name']
+    logging.info(print_str)
--- function_app.py
+++ function_app.py
@@ -3,6 +3,7 @@ import os
 import azure.functions as func
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import random
 
 app = func.FunctionApp()
 
@@ -26,5 +27,50 @@ def Appifier(myTimer: func.TimerRequest) -> None:
     logging.info('Replaced latest 20 songs')
     for idx, item in enumerate(results['items']):
         track = item['track']
-        print_str = str(idx) + track['artists'][0]['name'] + " - " + track['name']
+        print_str = str(idx) + " " + track['artists'][0]['name'] + " - " + track['name']
         logging.info(print_str)
+
+@app.timer_trigger(schedule="0 0 3 * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False)
+def Appifier2(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    scope = "user-library-read playlist-modify-private"
+    playlist_id_2 = os.getenv("PLAYLIST_ID_2")
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    # Make random list of 20 liked songs
+    liked_songs_count = 0
+    offset = 0
+    limit = 50
+    k = 20
+
+    while True:
+        results = sp.current_user_saved_tracks(offset=offset, limit=limit)
+        liked_songs_count += len(results['items'])
+        if results['next'] is None:
+            break
+        offset += limit
+
+    print(f"Number of liked songs: {liked_songs_count}")
+
+    population = range(0, liked_songs_count)
+    random_integers = random.sample(population, k)
+    tracks = []
+    track_ids = []
+
+    for rand_int in random_integers:
+        result = sp.current_user_saved_tracks(offset=rand_int, limit=1)
+        track = result['items'][0]['track']
+        tracks.append(track)
+        track_ids.append(track['id'])
+
+    sp.playlist_replace_items(playlist_id=playlist_id_2, items=track_ids)
+
+    logging.info('Updated random 20 songs')
+    for idx, track in enumerate(tracks):
+        print_str = str(idx) + " " + track['artists'][0]['name'] + " - " + track['name']
+        logging.info(print_str)
+
